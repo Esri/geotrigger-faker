e0765076dcef49fc704113d2e21b918b4745ee86
Nate Goldman <nnmgoldman@gmail.com>
2013-10-23 17:26:56 -0700

    i dont even

--- geofaker.js
+++ geofaker.js
@@ -65,7 +65,33 @@ function Geofaker (options) {
   this.session = new Geotriggers.Session({ clientId: this.clientId });
   this._sendQueue = [];
 
+  return this;
+
 }
+
+// set device tags
+// ---------------
+//
+// accepts an array of tags and a callback function
+//
+Geofaker.prototype.setTags = function (tags, callback) {
+
+  var request = {
+    // 'deviceIds': [this.deviceId],
+    'setTags': tags
+  };
+
+  this.session.queue(function(){
+    if (typeof callback === 'function') {
+      this.request('device/update', request, callback);
+    } else {
+      this.request('device/update', request);
+    }
+  });
+
+  return this;
+
+};
 
 // send location update
 // --------------------
@@ -83,30 +109,51 @@ function Geofaker (options) {
 //
 Geofaker.prototype.send = function (options, callback) {
 
-  var locations = [];
-  var type = Object.prototype.toString.call(options);
+  var locations = parseSendOptions(options);
 
-  if (type === '[object Object]') {
+  sendLocationUpdate.apply(this, [ { locations: locations }, callback ]);
+
+  return this;
+
+};
+
+// internal logic (private)
+// ------------------------
+
+function isObject (obj) {
+  return Object.prototype.toString.call(obj) === '[object Object]';
+}
+
+function isArray (obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+}
+
+function parseSendOptions (options) {
+  var locations = [];
+
+  if ( isObject(options) ) {
     locations.push( buildLocationObject(options) );
   }
 
-  else if (type === '[object Array]') {
+  else if ( isArray(options) && isObject(options[0]) ) {
     for (var i=0; i < options.length; i++) {
       locations.push( buildLocationObject(options[i]) );
     }
   }
 
-  else {
-    throw new Error('invalid parameter', options);
+  else if ( isArray(options) && options.length === 2 && !isNaN(options[0]) && !isNaN(options[1]) ) {
+    locations.push( buildLocationObject({
+      latitude: options[0],
+      longitude: options[1]
+    }) );
   }
 
-  var args = [ { locations: locations }, callback ];
+  else {
+    throw new Error('invalid location options', options);
+  }
 
-  sendLocationUpdate.apply(this, args);
+  return locations;
 }
-
-// internal logic (private)
-// ------------------------
 
 function buildLocationObject (locationObject) {
 
@@ -130,9 +177,13 @@ function sendLocationUpdate (locations, callback) {
 
   // send locations to `location/update` using Geofaker's device ID
   if (typeof callback === 'function') {
-    this.session.request('location/update', locations, callback);
+    this.session.queue(function(){
+      this.request('location/update', locations, callback);
+    });
   } else {
-    this.session.request('location/update', locations);
+    this.session.queue(function(){
+      this.request('location/update', locations);
+    });
   }
 
 }
